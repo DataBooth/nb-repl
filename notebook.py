6e4a390cddbc22d858698b916c98a0553d2d4ea4
Michael J. Booth <mjboothaus@gmail.com>
2021-09-20 11:10:55 +1000

    Create notebook.py

---
+++ notebook.py
@@ -0,0 +1,8 @@
+import os
+
+def run_notebook(dotipynb_filename=''):
+  print("Starting Jupyter notebook...")
+  print("")
+  print(f"jupyter notebook {dotipynb_filename} --ip 0.0.0.0 --port 3000")
+  print("")
+  os.system(f"jupyter notebook {dotipynb_filename} --ip 0.0.0.0 --port 3000")
